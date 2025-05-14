2a9d4b7be89cb4720788b28164a751fd082b243f
leowu <leo.wu@adidas.com>
2025-05-14 11:58:36 +0800

    update binaryTarget url

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
           ),
         .binaryTarget(
             name: "KMPShared",
-            url: "https://artifactory.tools.adidas.com.cn/artifactory/Flagship-android/cn/adidas/kmp/XCFramework/0.1.0/sharedKMP.xcframework.zip",
-            checksum: "36a86158607ab24d542609ad47df048cb346a6d178a7ecceaf6fb544f3ae45f8"
+            url: "https://artifactory.tools.3stripes.net/ui/native/MobileSDKRepo/cn/adidas/kmp/XCFramework/0.1.0/sharedKMP.xcframework.zip",
+            checksum: "2dca9597ef3b3cd3d043784700b7aaee0def41ab54f865ba7db2e1cd17876157"
         )
     ]
 )
