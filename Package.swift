ea947a07737540b72cb479f7b0e510656613365b
leowu <leo.wu@adidas.com>
2025-05-29 11:24:16 +0800

    update url

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
           ),
         .binaryTarget(
             name: "KMPShared",
-            url: "https://stg-adidasapp.api.adidas.com.cn/sdk/sharedKMP.xcframework.zip",
+            url: "https://stg-adidasapp.api.adidas.com.cn/sdk/ios/sharedKMP/0.1.0/sharedKMP.xcframework.zip",
             checksum: "36a86158607ab24d542609ad47df048cb346a6d178a7ecceaf6fb544f3ae45f8"
         )
     ]
